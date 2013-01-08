e3bcd2678315b841cd5509d3df3571d4f3223c62
Jason Brown <j.brown@ctidigital.com>
2013-01-08 15:04:41 +0000

    Added writer (getter) method to assign value to price

--- BookInStock.rb
+++ BookInStock.rb
@@ -11,8 +11,14 @@ class BookInStock
     "ISBN: #{@isbn}, price: #{@price}"
   end
 
+  def price=(new_price)
+    @price = new_price
+  end
+
 end
 
-book = BookInStock.new("isbn1", 3)
+book = BookInStock.new("isbn1", 33.80)
 puts "ISBN = #{book.isbn}"
 puts "Price = #{book.price}"
+book.price = book.price * 0.75
+puts "New price = #{book.price}"
