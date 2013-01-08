0b7dad3d3f3894ed019b6d8110bb0349ddfb1414
Jason Brown <j.brown@ctidigital.com>
2013-01-08 14:57:42 +0000

    Added Accessor methods

--- BookInStock.rb
+++ BookInStock.rb
@@ -7,11 +7,17 @@ class BookInStock
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
+
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
 end
 
-b1 = BookInStock.new("isbn1", 3)
-p b1
-b2 = BookInStock.new("isbn1", 3.14)
-p b2
-b3 = BookInStock.new("isbn1", "5.67")
-p b3
+book = BookInStock.new("isbn1", 3)
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
