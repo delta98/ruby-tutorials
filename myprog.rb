8c7155cf84bafdaca8df9b71efd5fac6e8a8f7de
Jason Brown <j.brown@ctidigital.com>
2013-01-08 12:04:51 +0000

    Array hashes using p to display values explicitly

--- myprog.rb
+++ myprog.rb
@@ -1,4 +1,23 @@
-a = [1, 'cat', 3.14]
-puts "The first element is #{a[0]}"
-a[2] = nil #represents null, but in ruby nil is an object
-puts "The array is now #{a.inspect}"
+a = ['ant', 'bee', 'cat', 'dog', 'elk']
+a[0] #=> "ant"
+a[3] #=> "dog"
+
+#this is the same:
+a = %w{ant bee cat dog elk} # %w allows you to create a hash/array without the need for '' or ,
+
+#ruby hashes are similar to arrays however you can specify a literal index instead of a numerical index
+
+inst_section = {
+    'cello' => 'string',
+    'clarinet' => 'woodwind',
+    'drum' => 'percussion',
+    'oboe' => 'woodwind',
+    'trumpet' => 'brass',
+    'violin' => 'string'
+}
+
+p inst_section['obe']
+p inst_section['cello']
+p inst_section['bassoon']
+
+# p works exactly like put except it displays values explicitly
